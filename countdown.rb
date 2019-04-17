4dec795510f9e3817cf16572a5bff444e76eb394
feven <feventsegay6@gmail.com>
2019-04-17 18:31:01 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(count)
+  
+  while count > 0
+  puts "#{count} SECOND(S)!"
+  count=count-1
+  end
+   "HAPPY NEW YEAR!"
+  end
+  
+   def countdown_with_sleep(count)
+    while count > 0
+      count-=1
+      sleep(1)
+    end
 end
+    
